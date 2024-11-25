d2cee7bad84a63c9d11cc419b29402773f2c8725
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-11-25 08:39:19 -0300

    devops: adicionando workflow para execução dos testes

---
+++ tests/test_module_payload.py
@@ -0,0 +1,63 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
+from src.modules.payload import ModulePayload
+from src.schemas.payload import SchemaPayload
+
+@pytest.fixture
+def module_payload() -> ModulePayload:
+    return ModulePayload()
+
+def test_get_payloads_from_redis(module_payload):
+    module_payload.get_payloads_from_redis()
+
+    assert isinstance(payloads) == list[SchemaPayload]
+
+def test_save_payload(module_payload):
+    sended_payload = {
+        "mac": "123",
+        "data": {
+            "temp": 40,
+            "umid": 3
+        },
+        "timestamp": 1729399539.969931
+    }
+    module_payload.save_payload(payload)
+    payloads = module_payload.get_payloads_from_redis()
+    for payload in payloads:
+        if payload.mac == sended_payload.mac:
+            assert sended_payload['mac'] == payload.mac
+            assert sended_payload['data'] == payload.data
+            assert sended_payload['timestamp'] == payload.timestamp
+            break
+
+def test_delete_payload(module_payload):
+    sended_payload = {
+        "mac": "123",
+        "data": {
+            "temp": 40,
+            "umid": 3
+        },
+        "timestamp": 1729399539.969931
+    }
+
+    module_payload.save_payload(sended_payload)
+
+    sended_payload = {
+        "mac": "124",
+        "data": {
+            "temp": 40,
+            "umid": 3
+        },
+        "timestamp": 1729399539.969931
+    }
+    
+    module_payload.save_payload(sended_payload)
+    module_payload.delete_payload(payload)
+
+    payloads = module_payload.get_payloads_from_redis()
+
+    assert isinstance(payloads) == list[SchemaPayload]
+    assert len(payloads) > 0
